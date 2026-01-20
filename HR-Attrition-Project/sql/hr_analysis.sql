94b627e3809a2c1491f31c5b14339f51894e683c
Puneeth-Raj-Y <puneeth.ys21@gmail.com>
2026-01-20 15:22:06 +0530

    Update

--- HR-Attrition-Project/sql/hr_analysis.sql
+++ HR-Attrition-Project/sql/hr_analysis.sql
@@ -71,3 +71,7 @@ ROUND(100.0 * SUM(CASE WHEN Attrition='Yes' THEN 1 ELSE 0 END) / COUNT(*),2)
 AS Attrition_Rate
 FROM hr_attrition
 GROUP BY OverTime;
+SHOW TABLES;
+SELECT * 
+FROM hr_attrition
+ORDER BY EmpID;
